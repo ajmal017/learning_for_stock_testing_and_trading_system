4b7d278602dcf7802c9f19e8532a4677d9ee5443
laoshu198838 <49549542+laoshu198838@users.noreply.github.com>
2020-01-30 22:30:52 +0800

    Update vn.py工作区.code-workspace

--- vnpy-2.0.9/vnpy/app/cta_strategy/strategies/art_rsi.py
+++ vnpy-2.0.9/vnpy/app/cta_strategy/strategies/art_rsi.py
@@ -1,5 +1,22 @@
-from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
+# from vnpy.app.cta_strategy.backtesting import BacktestingEngine, OptimizationSetting
 # from vnpy.app.cta_strategy.strategies.atr_rsi_strategy import AtrRsiStrategy
 # from vnpy.app.cta_strategy.strategies.boll_channel_strategy import BollChannelStrategy
+import test
+import Testing
+import Testing#导入多次和一次是一样的，因为模块不是用来打印的
+import sys,pprint
 from datetime import datetime
+#import tushare as ts
+import pandas as pd
+import numpy as np
+import datetime
+import os
+from datetime import datetime,timedelta
+#import oneday_data
+#import atr_rsi_strategy
 print('done')
+pprint.pprint(sys.path)
+
+sys.path.append('E:\\The Road For Finacial Statics\\Python\\02.Learning Materrials\\01.Programs')
+pprint.pprint(sys.path)
+#import oneday_data
